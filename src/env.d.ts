fdae6fd81b5558df29e19ac26758f6731db97484
novusweb3 <adrian@ithd.co.uk>
2024-11-21 13:14:11 +0000

    sorted dependencies errors

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly EMAIL_USER: string;
+  readonly EMAIL_PASS: string;
+  // ... other env variables
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
